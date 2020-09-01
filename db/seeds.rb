7c67532e0cad5d786cb44401cc1ab1da5f89eaba
Harris Rahim <harris.rhm@gmail.com>
2020-09-01 20:14:04 +1000

    wikinous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Article.destroy_all
+puts "creating 10 articles"
+10.times do 
+    Article.create(
+        title: Faker::DcComics.hero,
+        content: Faker::DcComics.title
+    )
+end
+puts "created #{Article.count} articles"
